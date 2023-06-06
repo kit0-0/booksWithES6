8d68ad103e53f511ecec140f89dbadfd690eaeba
kit0-0 <kt1767@gmail.com>
2023-06-07 00:46:29 +0530

    Add nav.js

---
+++ modules/nav.js
@@ -0,0 +1,21 @@
+export function showContent(sectionId) {
+  const sections = document.getElementsByClassName('content');
+
+  for (let i = 0; i < sections.length; i += 1) {
+    sections[i].style.display = 'none';
+  }
+
+  const selectedSection = document.getElementById(sectionId);
+  selectedSection.style.display = 'block';
+}
+
+export function setupNavigation() {
+  const links = document.querySelectorAll('.navbar a');
+  links.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const sectionId = link.getAttribute('href').substring(1);
+      showContent(sectionId);
+    });
+  });
+}
